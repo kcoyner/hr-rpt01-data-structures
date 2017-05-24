977107a3d0db4883afb3e545be3239718606752a
Kevin Coyner <kevin@rustybear.com>
2017-05-24 16:37:19 -0400

    doodle: Completed functional queue.js.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var back = 0;
+  var front = 0;
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage[back] = value;
+    return back++;
   };
 
   someInstance.dequeue = function() {
+  // must move up front after deleting because
+  // only the property is deleted not the key
+    if (back > front) {
+      var result = storage[front];
+      delete storage[front];
+      front++;
+      return result;
+    }
   };
 
   someInstance.size = function() {
+    return back - front;
   };
 
   return someInstance;
