96b9e2c3de6c45b15ea3f8ffac370e11b4942e46
Kevin Coyner <kevin@rustybear.com>
2017-05-24 23:11:35 -0400

    doodle: doodle: Completed pseudo classical stack.js.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.stackSize = 0;
 };
 
+Stack.prototype.size = function () {
+  return this.stackSize;
+};
 
+Stack.prototype.push = function (value) {
+  this.stackSize++;
+  this.storage[this.stackSize] = value;
+  return this.stackSize;
+};
+
+Stack.prototype.pop = function () {
+  if (this.stackSize > 0) {
+    var popped = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    this.stackSize--;
+    return popped;
+  }
+};
