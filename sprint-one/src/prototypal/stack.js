a87f54abd332716f4b8860ab2da72aaa9697d02e
Kevin Coyner <kevin@rustybear.com>
2017-05-24 22:22:34 -0400

    doodle: doodle: Completed prototypal stack.js.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.stackSize = 0;
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function () {
+  return this.stackSize;
+};
+
+stackMethods.push = function (value) {
+  this.stackSize++;
+  this.storage[this.stackSize] = value;
+  return this.stackSize;
+};
+
+stackMethods.pop = function () {
+  if (this.size() > 0) {
+    var popped = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    this.stackSize--;
+    return popped;
+  }
+};
 
