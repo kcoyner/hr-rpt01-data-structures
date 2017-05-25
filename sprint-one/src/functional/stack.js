b25f2626fa93fbbd7c4a6359d7b2d8877a21cd55
Kevin Coyner <kevin@rustybear.com>
2017-05-24 20:42:19 -0400

    doodle: doodle: Complete functional - stack.js.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,29 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
+    storage[size] = value;
+    return size;
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      var popped = storage[size];
+      delete storage[size];
+      size--;
+      return popped;
+    }
   };
 
   someInstance.size = function() {
+    return size;
+
   };
 
   return someInstance;
 };
+
