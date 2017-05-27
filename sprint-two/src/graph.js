7163f58602b6bbd3013e911a74d459a044202fa0
Kevin Coyner <rustybear@gmail.com>
2017-05-27 19:52:32 -0400

    Partial complete through node remove.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,32 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.storage = {};
 };
+
+// {3: []}
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  if (this.storage.hasOwnProperty(node) === false) {
+    this.storage[node] = [node];
+  }
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (this.storage.hasOwnProperty(node) === true) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  if (this.storage.hasOwnProperty(node) === true) {
+    delete this.storage[node];
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
