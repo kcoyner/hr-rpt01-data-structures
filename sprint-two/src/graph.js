ea7a407e72736d66a95377d559c73a177f1de0ef
Kevin Coyner <rustybear@gmail.com>
2017-05-27 21:37:51 -0400

    Complete through graph.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -8,6 +8,7 @@ var Graph = function() {
 // {3: [3]}
 
 // Add a node to the graph, passing in the node's value.
+// Time complexity:  constant time
 Graph.prototype.addNode = function(node) {
   if (this.storage.hasOwnProperty(node) === false) {
     this.storage[node] = [node];
@@ -15,6 +16,7 @@ Graph.prototype.addNode = function(node) {
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
+// Time complexity:  O(1) constant time
 Graph.prototype.contains = function(node) {
   if (this.storage.hasOwnProperty(node) === true) {
     return true;
@@ -24,28 +26,32 @@ Graph.prototype.contains = function(node) {
 };
 
 // Removes a node from the graph.
+// Time complexity: quardratic time O(n2).
 Graph.prototype.removeNode = function(node) {
   if (this.storage.hasOwnProperty(node) === true) {
     var temp = this.storage[node];
-    for (let i = 1; i < temp.length; i++) {
-      this.removeEdge(node, temp[i]);
+    for (let i = 1; i < temp.length; i++) { // 0(n)
+      this.removeEdge(node, temp[i]); // 0(n)
     }
     delete this.storage[node];
   }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
+// Time complexity:  O(n) linear time
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   return this.storage[fromNode].includes(toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
+// Time complexity:  O(1) constant time
 Graph.prototype.addEdge = function(fromNode, toNode) {
   this.storage[fromNode].push(toNode);
   this.storage[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
+// Time complexity:  O(n) linear time
 Graph.prototype.removeEdge = function(fromNode, toNode) {
   var target = this.storage[fromNode].indexOf(toNode);
   this.storage[fromNode].splice(target, 1);
@@ -54,7 +60,11 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 };
 
 // Pass in a callback which will be executed on each node of the graph.
+// Time complexity:  O(n) linear time
 Graph.prototype.forEachNode = function(cb) {
+  for (var node in this.storage) {
+    cb(node);
+  }
 };
 
 /*
