5d3058af5a449be574a519fb49822f8d26d1c511
Kevin Coyner <rustybear@gmail.com>
2017-05-27 20:22:49 -0400

    Partial complete through removeEdge.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -5,7 +5,7 @@ var Graph = function() {
   this.storage = {};
 };
 
-// {3: []}
+// {3: [3]}
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
@@ -32,14 +32,21 @@ Graph.prototype.removeNode = function(node) {
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.storage[fromNode].includes(toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.storage[fromNode].push(toNode);
+  this.storage[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var target = this.storage[fromNode].indexOf(toNode);
+  this.storage[fromNode].splice(target, 1);
+  target = this.storage[toNode].indexOf(fromNode);
+  this.storage[toNode].splice(target, 1);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
