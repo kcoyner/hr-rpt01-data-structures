64bf87eaa9524d7a19ad47bdfa8f3147e608d8af
Kevin Coyner <rustybear@gmail.com>
2017-05-29 22:06:58 -0400

    Working except for last test on collisions.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,28 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  console.log('this.: ', this._storage.get(index));
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // console.log('index: ', index);
+  this._storage.set(index, undefined);
+
+  // this._storage.each(function (target) {
+  //   // k is index position.  remove it or splice it
+  //   if (target) {
+  //     // console.log('k: ', k);
+  //     target = undefined;
+  //   }
+  // });
+
 };
 
 
