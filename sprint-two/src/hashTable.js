9b73246f79cd6ef2dbe387d028029a0d4277ff76
Kevin Coyner <rustybear@gmail.com>
2017-05-29 23:25:31 -0400

    Continue to troubleshoot last test. No success.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,26 +7,28 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // must add to bucket array but presently it overwrites
+  // i tried wrapping k,v in a function that pushed into storage[]
+  // because this is set:  storage[index] = value;
   this._storage.set(index, [k, v]);
+  // this._storage.set(index, function (k,v) { storage.push([k,v]); });
+  console.log('this._storage: ', this._storage);
+  // this._storage is an object with storage being an array and 3 methods
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   // must loop thru to find proper value
+  // console.log('index: ', index);
+  this._storage.each(function (index) {
+    console.log('index: ', index);
+  });
   return this._storage.get(index)[1];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   this._storage.set(index, [k, undefined]);
-
-  // this._storage.each(function (target) {
-  //   // k is index position.  remove it or splice it
-  //   if (target) {
-  //     // console.log('k: ', k);
-  //     target = undefined;
-  //   }
-  // });
 
 };
 
