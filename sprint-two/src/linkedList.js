aa011d9865dc02d22806b739796088384be3c891
Kevin Coyner <rustybear@gmail.com>
2017-05-30 15:43:02 -0400

    Added completed code for linkedList from master.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,49 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    if (!list.head) {
+      list.head = newNode;
+    }
+
+    if (list.tail) {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (!list.head) {
+      return null;
+    } else {
+      var oldHead = list.head;
+      delete list.head;
+      list.head = oldHead.next;
+      return oldHead.value;
+    }
   };
 
   list.contains = function(target) {
+
+    // if (list.head.value === target) {
+    //   return true;
+    // }
+
+    var recurse = function (node, target) {
+      if (node.value === target) {
+        return true;
+      } else if (node.next) {
+        return recurse(node.next, target);
+      } else {
+        return false;
+      }
+    };
+
+    return recurse(list.head, target);
+
   };
 
   return list;
@@ -24,6 +61,5 @@ var Node = function(value) {
   return node;
 };
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+
+
