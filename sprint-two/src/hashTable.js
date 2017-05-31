5285952e1ba77f6c8f6c80c2481de82862d2c441
Kevin Coyner <rustybear@gmail.com>
2017-05-31 00:40:31 -0400

    Partial completion on hashTable.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,10 +7,17 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // check to see if index has an entry already
+  // if it has an entry, we'll rebuild the v argument
+  // this._storage.set(index, [[k, v],[k2,v2],[k3,v3]]); // [k,v]
+  var tempArr = [];
+  tempArr.push(this._storage.get(index));
+  this._storage.set(index, [k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[1];
 };
 
 HashTable.prototype.remove = function(k) {
@@ -22,5 +29,6 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
 
 
