70ed08f41010691b25be0c992f468c7507aa751c
Kevin Coyner <rustybear@gmail.com>
2017-05-30 23:28:04 -0400

    Completed set.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,36 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
+// Time: Linear
 setPrototype.add = function(item) {
+  if (this.contains(item) === false){
+    this._storage.push(item);
+  }
+  return this
 };
 
+// Time: Linear
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
+// Time: Linear
 setPrototype.remove = function(item) {
+  for (let i = 0; i < this._storage.length; i++) {
+    if ( this._storage[i].includes(item) === true) {
+      this._storage.splice(i, 1);
+      return true;
+    }
+  }
+  return false;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
