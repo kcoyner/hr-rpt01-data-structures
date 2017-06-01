4ea1255afa0299e33f153f0da1d7a74c37f2c856
Kevin Coyner <rustybear@gmail.com>
2017-06-01 15:03:01 -0400

    Completed basic part of hashTable.js.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,4 @@
 
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,29 +6,34 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // must add to bucket array but presently it overwrites
-  // i tried wrapping k,v in a function that pushed into storage[]
-  // because this is set:  storage[index] = value;
-  this._storage.set(index, [k, v]);
-  // this._storage.set(index, function (k,v) { storage.push([k,v]); });
-  console.log('this._storage: ', this._storage);
-  // this._storage is an object with storage being an array and 3 methods
+  if (this._storage.get(index)) {
+    let prevTuple = this._storage.get(index);
+    this._storage.set(index, [k, v].concat(prevTuple));
+  } else {
+    this._storage.set(index, [k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // must loop thru to find proper value
-  // console.log('index: ', index);
-  this._storage.each(function (index) {
-    console.log('index: ', index);
+  let tuples = this._storage.get(index);
+
+  this._storage.each(function (elem) {
+    // how to use this? elem is an array
+    // console.log('elem: ', (elem));
+    // console.log('elem: ', Array.isArray(elem));
   });
-  return this._storage.get(index)[1];
+
+  for (let i = 0; i < tuples.length; i++) {
+    if (k === tuples[i]) {
+      return tuples[i + 1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   this._storage.set(index, [k, undefined]);
-
 };
 
 
@@ -37,5 +41,6 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
 
 
