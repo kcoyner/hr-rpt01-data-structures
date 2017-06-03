991a3725e89e18d942839fede1942897dd848218
Kevin Coyner <rustybear@gmail.com>
2017-06-02 21:03:29 -0400

    doodle: doodle: Progress capture with working insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,78 @@
 var BinarySearchTree = function(value) {
+
+
+  var tree = new Node(value);
+
+  var recurse = function (node, target) {
+    if (target > node.value) {
+      if (node.right === null) {
+        node.right = new Node(target);
+      } else {
+        recurse(node.right, target);
+      }
+    }
+    if (target < node.value) {
+      if (node.left === null) {
+        node.left = new Node(target);
+      } else {
+        recurse(node.left, target);
+      }
+    }
+  };
+
+  tree.insert = function(value) {
+    recurse(tree, value);
+  };
+
+  var recurseContains = function (node, target) {
+    if (target > node.value) {
+      if (node.right === null) {
+        node.right = new Node(target);
+      } else {
+        recurse(node.right, target);
+      }
+    }
+    if (target < node.value) {
+      if (node.left === null) {
+        node.left = new Node(target);
+      } else {
+        recurse(node.left, target);
+      }
+    }
+  };
+
+  tree.contains = function(value) {
+    // returns true or false
+    recurseContains(tree, value);
+  };
+
+  tree.depthFirstLog = function(value) {
+    var arr = [];
+    //arr.push(everyvalue)
+    // returns an array
+  };
+
+  console.log('tree: ', tree);
+  return tree;
+
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.right = null;
+  node.left = null;
+
+  return node;
+};
+
+
+
