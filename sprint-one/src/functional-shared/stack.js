e65e867b32941bef39cb4386e0145379739eaf6c
Kevin Coyner <rustybear@gmail.com>
2017-06-02 22:59:09 -0400

    Completed sprint one.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,7 +1,4 @@
-
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   var newStack = {};
   newStack.stackSize = 0;
   newStack.storage = {};
